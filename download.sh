c92fc7374616e7042f7fb175f93b7a847025ca19
Sanshinkai Aikido Utrecht <utrecht@sanshinkai.eu>
2015-05-04 11:50:04 +0200

    First full upload for 2013 and 2015.

--- download.sh
+++ download.sh
@@ -1,8 +1,28 @@
-if [ ! -e Yoritsuki ]
+exit
+set -xe
+if [ ! -e icons ]
 then
-	if [ ! -e yoritsuki_win.zip ]
+	if [ ! -e Yoritsuki ]
 	then
-		wget http://www.hybridworks.jp/apps/yoritsuki/gifts/yoritsuki_win.zip
+		if [ ! -e yoritsuki_win.zip ]
+		then
+			wget http://www.hybridworks.jp/apps/yoritsuki/gifts/yoritsuki_win.zip
+		fi
+		unzip yoritsuki_win.zip
 	fi
-	unzip yoritsuki_win.zip
+	chmod u+rw -R Yoritsuki
+	mkdir icons
+	mv 'Yoritsuki/Icons(png file)' icons/transparent
+	mkdir -p icons/transparent
+	cd icons/transparent
+	for i in *.png
+	do
+		ls "$i"
+		i=$(printf '%q' "$i")
+		pwd
+		ls '"'$i'"'
+		convert "$i" -background white -flatten ../white/"$i"
+	done
+	cd ../..
+	rm -rf Yoritsuki
 fi
